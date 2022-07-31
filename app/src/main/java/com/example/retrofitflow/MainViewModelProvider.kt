68d77681cf8b39fd9d3240354361cf1b7f89337e
Ayan Debnath <rintu98deb@gmail.com>
2022-07-31 07:36:15 +0530

    Changes in MainViewModel

--- app/src/main/java/com/example/retrofitflow/MainViewModelProvider.kt
+++ app/src/main/java/com/example/retrofitflow/MainViewModelProvider.kt
@@ -3,8 +3,8 @@ package com.example.retrofitflow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class MainViewModelProvider(val str: String) : ViewModelProvider.Factory {
+class MainViewModelProvider(private val str: String) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return super.create(modelClass)
+        return MainViewModel(str) as T
     }
 }
