da7cae8661ea1b9d2488227fc28e595014e1a6f9
Ayan Debnath <rintu98deb@gmail.com>
2022-07-31 07:51:23 +0530

    view model using delegate property

--- app/src/main/java/com/example/retrofitflow/MainActivity.kt
+++ app/src/main/java/com/example/retrofitflow/MainActivity.kt
@@ -2,16 +2,16 @@ package com.example.retrofitflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mainViewModel: MainViewModel
+
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val viewModelProvider = MainViewModelProvider("some string")
-        mainViewModel = ViewModelProvider(this, viewModelProvider)[MainViewModel::class.java]
 
     }
 }
